0c91e6a5e29b53ae672bf48d89e1eba11fc75562
Noopur Bhatt <nbhatt2020@pingry.org>
2017-04-28 14:16:22 -0400

    Created another test.

--- test.py
+++ test.py
@@ -3,3 +3,4 @@ import palindrome
 print(palindrome.is_palindrome("madam"))
 print(palindrome.is_palindrome("racecar"))
 print(palindrome.is_palindrome("nurses run far away"))
+print(palindrome.is_palindrome("noel sees leon"))
