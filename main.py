3eb26dba7513c3ba3d897c996a45c93416f93163
sburak13 <sburak2020@pingry.org>
2017-05-02 12:41:53 -0400

    Update main.py
    Added comments to main program

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import palindrome
-
+# Ask the user for a input.
 question = raw_input("Yo my guy, dish me a string and I will tell you if it is a palindrome or not! ")
 
+# Test the input
 print(palindrome.is_palindrome(question))
